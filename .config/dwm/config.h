d8c20622e68506cd9244a4d50ee369e9b0f31a06
ayekat <takeya@bluewin.ch>
2013-03-16 16:58:03 +0100

    Changed symbol for scratchpad.
    * Alright, basically removed the symbol.
    * Adapted dzenconky size, as a concequence.

--- .config/dwm/config.h
+++ .config/dwm/config.h
@@ -33,7 +33,7 @@ static const Bool showbar           = True;     /* False means no bar */
 static const Bool topbar            = True;     /* False means bottom bar */
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "", "", "∫", "", "", "", "" };
+static const char *tags[] = { "1", "2", "3", "", "", "∫", "", "", "", " " };
 //#include "shiftview.c" // cycle through tags
 
 static const Rule rules[] = {
