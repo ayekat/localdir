b59e764539a49bffc9031272ffc70128b9dcabc5
ayekat <takeya@bluewin.ch>
2013-04-18 13:18:03 +0200

    Changed keybind for closing windows in dwm.
    * Alt+F4 is horrible.
    * Updated tag names 1-3.

--- .config/dwm/config.h
+++ .config/dwm/config.h
@@ -1,12 +1,13 @@
+/* See LICENSE file for copyright and license details. */
+
 #include "bstack.c"         //     "        "    "  split screen vertically
 #include "fibonacci.c"      //     "        "    "  arrange as Fibonacci
 #include "push.c"           // move clients around in stack/master
 
-/* See LICENSE file for copyright and license details. */
-
 /* appearance */
 static const char font[]            = "Fixed Medium Semi-Condensed 10";
 //static const char font[]            = "-misc-fixed-medium-r-semicondensed--13-100-100-100-c-60-iso8859-1";
+
 // orange
 static const char normbordercolor[]      = "#222222";
 static const char selbordercolor[]       = "#E04613";
@@ -14,6 +15,7 @@ static const char normbgcolor[]          = "#222222";
 static const char normfgcolor[]          = "#AAAAAA";
 static const char selbgcolor[]           = "#E04613";
 static const char selfgcolor[]           = "#FFFFFF";
+
 /*
 // blue
 static const char normbordercolor[] = "#222222";
@@ -34,9 +36,9 @@ static const Bool resizehints     = False;/* True means respect size hints in ti
 
 /* tagging */
 static const char *tags[] = {
-	"1",
-	"2",
-	"3",
+	"_",
+	"_",
+	"_",
 	"∫",
 	"∫",
 	"∫",
@@ -64,17 +66,18 @@ static const Rule rules[] = {
 /* layout(s) */
 static const Layout layouts[] = {
 	/* symbol     arrange function */
-	{ "",      tile },    /* first entry is default */
-	{ "",      bstack },
+	{ "",        tile },    /* first entry is default */
+	{ "",        bstack },
 	{ "[M]",      monocle },
-	{ "",      dwindle },
+	{ "",        dwindle },
 	{ .symbol = NULL,   .arrange = NULL    }, /* for cycling (see below) */
 };
 
 /* cycling through layouts (provided by bob127) here:
  * https://bbs.archlinux.org/viewtopic.php?pid=817499#p817499
  */
-void nextlayout(const Arg *arg)
+void
+nextlayout(const Arg *arg)
 {
 	Layout *l;
 	for (l=(Layout *)layouts;l != selmon->lt[selmon->sellt];l++);
@@ -83,7 +86,8 @@ void nextlayout(const Arg *arg)
 	else
 		setlayout(&((Arg) { .v = layouts }));
 }
-void prevlayout(const Arg *arg)
+void
+prevlayout(const Arg *arg)
 {
 	Layout *l;
 	for (l=(Layout *)layouts;l != selmon->lt[selmon->sellt];l++);
@@ -126,7 +130,8 @@ shiftview(const Arg *arg) {
 
 /* Moves and follows window to the next tag.
  */
-void movefollowtag(const Arg *arg)
+void
+movefollowtag(const Arg *arg)
 {
 	Arg shifted;
 
@@ -144,7 +149,8 @@ void movefollowtag(const Arg *arg)
 
 /* Moves and follows a window to the next monitor.
  */
-void movefollowmon(const Arg *arg)
+void
+movefollowmon(const Arg *arg)
 {
 	tagmon(arg);
 	focusmon(arg);
@@ -175,81 +181,87 @@ focusview(const Arg *arg)
 /* helper for spawning shell commands in the pre dwm-5.0 fashion */
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
 
-/* commands */
-static const char *dmenucmd[] = { "dmenu_run", "-l", "10", "-b", "-fn", font,
-		"-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbordercolor, "-sf",
-		selfgcolor, "-p", "<>", NULL };
+/* terminal commands */
 static const char *termcmd[]  = { "xfce4-terminal", NULL };
 static const char *termcmd_new[]  = { "urxvtc", NULL };
+static const char *scratchpadcmd[] = { "scratchpad", NULL };
+
+/* session commands */
 static const char *quitcmd[] = { "killall", "dwmloop", NULL };
 static const char *lockcmd[] = { "slock", NULL };
+
+/* hardware commands */
 static const char *volraisecmd[] = { "amixer", "set", "Master", "3+", "unmute", NULL };
 static const char *vollowercmd[] = { "amixer", "set", "Master", "3-", "unmute", NULL };
 static const char *volmutecmd[] = { "amixer", "set", "Master", "toggle", NULL };
-static const char *prtscrcmd[] = { "scrot", "/home/ayekat/img/screenshots/%Y-%m-%d_%H%M%S.png", NULL };
-static const char *dzenconkycmd[] = { "dzenconky", "restart", NULL };
-static const char *redshiftcmd[] = { "redshifttoggle", NULL };
-static const char *scratchpadcmd[] = { "scratchpad", NULL };
 static const char *lcdupcmd[] = { "sudo", "lcdctl", "increase", NULL };
 static const char *lcddowncmd[] = { "sudo", "lcdctl", "decrease", NULL };
 static const char *kbdupcmd[] = { "sudo", "lcdctl", "increase", "keyboard", NULL };
 static const char *kbddowncmd[] = { "sudo", "lcdctl", "decrease", "keyboard", NULL };
-static const char *timetablecmd[] = { "firefox", "http://localhost/timetable.png", NULL };
 
+/* miscellaneous commands */
+static const char *prtscrcmd[] = { "scrot", "/home/ayekat/img/screenshots/%Y-%m-%d_%H%M%S.png", NULL };
+static const char *dzenconkycmd[] = { "dzenconky", "restart", NULL };
+static const char *redshiftcmd[] = { "redshifttoggle", NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-l", "8", "-b", "-fn", font,
+		"-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbgcolor, "-sf",
+		selfgcolor, "-p", "$", NULL };
+
+/* key definitions */
 static Key keys[] = {
-	/* modifier           key        function        argument */
-	{ MODKEY,             XK_p,      spawn,          {.v = dmenucmd } },
-	{ 0,                  XK_Print,  spawn,          {.v = prtscrcmd } },
-	{ MODKEY,             XK_b,      spawn,          {.v = dzenconkycmd } },
-	{ MODKEY,             XK_r,      spawn,          {.v = redshiftcmd } },
-	{ MODKEY,             XK_F4,     killclient,     {0} },
+	/* modifier           key         function       argument */
+	{ MODKEY,             XK_p,       spawn,         {.v = dmenucmd } },
+	{ 0,                  XK_Print,   spawn,         {.v = prtscrcmd } },
+	{ MODKEY,             XK_b,       spawn,         {.v = dzenconkycmd } },
+	{ MODKEY,             XK_r,       spawn,         {.v = redshiftcmd } },
+	{ MODKEY|ShiftMask,   XK_i,       killclient,    {0} },
 
 	// terminal:
-	{ MODKEY|ControlMask, XK_j,      spawn,          {.v = termcmd } },
-	{ MODKEY|ControlMask, XK_n,      spawn,          {.v = termcmd_new } },
+	{ MODKEY|ControlMask, XK_j,       spawn,         {.v = termcmd } },
+	{ MODKEY|ControlMask, XK_n,       spawn,         {.v = termcmd_new } },
 
 	// scratchpad:
-	{ MODKEY,             XK_Tab,    focusview,      {.ui=1<<(LENGTH(tags)-1)}},
-	{ MODKEY|ShiftMask,   XK_Tab,    spawn,          {.v = scratchpadcmd } },
+	{ MODKEY,             XK_Tab,     focusview,     {.ui=1<<(LENGTH(tags)-1)}},
+	{ MODKEY|ShiftMask,   XK_Tab,     spawn,         {.v = scratchpadcmd } },
 
 	// volume keys:
-	{ 0,                  0x1008FF11,spawn,          {.v = vollowercmd } },
-	{ MODKEY|ControlMask, XK_Down,   spawn,          {.v = vollowercmd } },
-	{ 0,                  0x1008FF12,spawn,          {.v = volmutecmd } },
-	{ MODKEY|ControlMask, XK_m,      spawn,          {.v = volmutecmd } },
-	{ 0,                  0x1008FF13,spawn,          {.v = volraisecmd } },
-	{ MODKEY|ControlMask, XK_Up,     spawn,          {.v = volraisecmd } },
+	{ 0,                  0x1008FF11, spawn,         {.v = vollowercmd } },
+	{ MODKEY|ControlMask, XK_Down,    spawn,         {.v = vollowercmd } },
+	{ 0,                  0x1008FF12, spawn,         {.v = volmutecmd } },
+	{ MODKEY|ControlMask, XK_m,       spawn,         {.v = volmutecmd } },
+	{ 0,                  0x1008FF13, spawn,         {.v = volraisecmd } },
+	{ MODKEY|ControlMask, XK_Up,      spawn,         {.v = volraisecmd } },
 
 	// brightness keys (only used for MacBookPro):
-	{ 0,                  0x1008FF02,spawn,          {.v = lcdupcmd } },
-	{ 0,                  0x1008FF03,spawn,          {.v = lcddowncmd } },
-	{ 0,                  0x1008FF05,spawn,          {.v = kbdupcmd } },
-	{ 0,                  0x1008FF06,spawn,          {.v = kbddowncmd } },
+	{ 0,                  0x1008FF02, spawn,         {.v = lcdupcmd } },
+	{ 0,                  0x1008FF03, spawn,         {.v = lcddowncmd } },
+	{ 0,                  0x1008FF05, spawn,         {.v = kbdupcmd } },
+	{ 0,                  0x1008FF06, spawn,         {.v = kbddowncmd } },
 
 	// clients:
-	{ MODKEY,             XK_j,      focusstack,     {.i = +1 } },
-	{ MODKEY,             XK_k,      focusstack,     {.i = -1 } },
-	{ MODKEY|ShiftMask,   XK_j,      pushdown,       {0} },
-	{ MODKEY|ShiftMask,   XK_k,      pushup,         {0} },
-	{ MODKEY,             XK_h,      setmfact,       {.f = -0.02} },
-	{ MODKEY,             XK_l,      setmfact,       {.f = +0.02} },
-	{ MODKEY,             XK_Return, zoom,           {0} },
-	//{ Mod5Mask,           XK_Return, zoom,           {0} },
+	{ MODKEY,             XK_j,       focusstack,    {.i = +1 } },
+	{ MODKEY,             XK_k,       focusstack,    {.i = -1 } },
+	{ MODKEY|ShiftMask,   XK_j,       pushdown,      {0} },
+	{ MODKEY|ShiftMask,   XK_k,       pushup,        {0} },
+	{ MODKEY,             XK_h,       setmfact,      {.f = -0.02} },
+	{ MODKEY,             XK_l,       setmfact,      {.f = +0.02} },
+	{ MODKEY,             XK_Return,  zoom,          {0} },
+	//{ Mod5Mask,           XK_Return,  zoom,          {0} },
 
 	// layouts:
-	{ MODKEY,             XK_comma,  incnmaster,     {.i = +1 } },
-	{ MODKEY,             XK_period, incnmaster,     {.i = -1 } },
-	{ MODKEY,             XK_space,  nextlayout,     {0} },
-	{ MODKEY|ShiftMask,   XK_space,  prevlayout,     {0} },
-	{ MODKEY,             XK_t,      togglefloating, {0} },
+	{ MODKEY,             XK_comma,   incnmaster,    {.i = +1 } },
+	{ MODKEY,             XK_period,  incnmaster,    {.i = -1 } },
+	{ MODKEY,             XK_space,   nextlayout,    {0} },
+	{ MODKEY|ShiftMask,   XK_space,   prevlayout,    {0} },
+	{ MODKEY,             XK_t,       togglefloating,{0} },
 
 	// tags:
-	{ MODKEY|ControlMask, XK_l,      shiftview,      {.i = +1 } },
-	{ MODKEY|ControlMask, XK_h,      shiftview,      {.i = -1 } },
-	{ MODKEY|ControlMask|ShiftMask,XK_l,movefollowtag,{.i = +1 } },
-	{ MODKEY|ControlMask|ShiftMask,XK_h,movefollowtag,{.i = -1 } },
-	{ MODKEY,             XK_0,      view,           {.ui = ~0 } },
-	{ MODKEY|ShiftMask,   XK_0,      tag,            {.ui = ~0 } },
+	{ MODKEY|ControlMask,           XK_l,      shiftview,     {.i = +1 } },
+	{ MODKEY|ControlMask,           XK_h,      shiftview,     {.i = -1 } },
+	{ MODKEY|ControlMask|ShiftMask, XK_l,      movefollowtag, {.i = +1 } },
+	{ MODKEY|ControlMask|ShiftMask, XK_h,      movefollowtag, {.i = -1 } },
+	{ MODKEY,                       XK_0,      view,          {.ui = ~0 } },
+	{ MODKEY|ShiftMask,             XK_0,      tag,           {.ui = ~0 } },
 
 	// monitors:
 	{ MODKEY,                       XK_m,      focusmon,      {.i = +1 } },
@@ -274,9 +286,6 @@ static Key keys[] = {
 	{ MODKEY,                       XK_z, spawn, {.v = lockcmd } },
 	{ MODKEY|ShiftMask,             XK_z, quit,  {0} },
 	{ MODKEY|ControlMask|ShiftMask, XK_z, spawn, {.v = quitcmd } },
-
-	// show timetable:
-	{ MODKEY|ControlMask, XK_t,     spawn,       {.v = timetablecmd } },
 };
 
 /* button definitions */
