e1e14d5bb4ee567a4b725838250c5e4fefb02f35
ayekat <takeya@bluewin.ch>
2016-10-13 16:57:36 +0200

    Make python write history file to XDG-conform location

---
+++ .local/lib/python/startup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+# This script is run at the startup of each interactive python console when
+# defined in PYTHONSTARTUP.
+
+import readline
+import atexit
+import os
+
+python_history = os.environ.get('XDG_LOG_HOME', os.environ.get('XDG_CACHE_HOME', None))
+if python_history:
+    python_history = '%s/python_history' % python_history
+else:
+    python_history = '%s/.python_history' % os.environ['HOME']
+
+print('Using history file %s' % python_history)
+if os.path.exists(python_history):
+    readline.read_history_file(python_history)
+atexit.unregister(readline.write_history_file)
+atexit.register(readline.write_history_file, python_history)
