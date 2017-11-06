a049c559a44f7c3246fb70d2356d50aa6297f871
ayekat <takeya@bluewin.ch>
2017-11-06 18:32:34 +0100

    etc/X11: Move ibus launching into .d/.sh snippet

--- etc/X11/xinitrc.d/ibus.sh
+++ etc/X11/xinitrc.d/ibus.sh
@@ -1,3 +1,6 @@
-export GTK_IM_MODULE=ibus
-export QT_IM_MODULE=ibus
-export XMODIFIERS='@im=ibus'
+if which ibus-daemon >/dev/null 2>&1; then
+	export GTK_IM_MODULE=ibus
+	export QT_IM_MODULE=ibus
+	export XMODIFIERS='@im=ibus'
+	ibus-daemon -drx
+fi
