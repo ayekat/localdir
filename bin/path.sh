531ac6ee14f6c3d4617dbfa9b56b07646bbca494
ayekat <takeya@bluewin.ch>
2017-11-30 16:09:34 +0100

    bin: Add `unwrap` script for... wrapper scripts
    It's a bit easier on the eye than having a script source a random shell
    script snippet with a weird line of sed and an environment variable set.

--- bin/path.sh
+++
@@ -1,5 +0,0 @@
-# This shell snippet sets the `EPATH` variable to the next location of the
-# executable that has the same name as the currently running executable.
-# Handy for using in "wrapper" scripts.
-
-EPATH="$(PATH="$(echo ":$PATH:" | sed -e "s|:\\($(dirname "$0"):\\)\\+|:|g;s|^:||;s|:\$||")" which "$(basename "$0")")"
