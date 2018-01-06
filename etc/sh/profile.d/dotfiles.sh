d4fc8201ee8531c5884e6357e03c0a5d1695fcd0
ayekat <takeya@bluewin.ch>
2018-01-06 15:18:51 +0100

    etc/sh: Fix PATH buildup in profile files
    Given the new filesystem package in Arch Linux, this should probably go
    into `etc/sh/environment`. But then again...
    
    There is currently no single, standard, correct location to set
    environment variables on Linux. There is ~/.pam_environment, whose
    horrible and limited syntax doesn't even allow for simple things like
    "append/prepend something to a variable"; then there is systemd's
    systemd-environment-d-generator, which suffers from a chicken-and-egg
    problem (and "solves" this by assuming XDG_CONFIG_HOME is ~/.config,
    because fuckyall); then there are shell profile files, which are the
    most flexible, but do not apply to graphical logins (e.g. through a
    display manager) and systemd user instances.
    
    And so far, nobody could tell me where environment variables should be
    defined. It's an issue nobody really seems to care about except for
    people who are unhappy with it.
    
    Yay.

--- etc/sh/profile.d/dotfiles.sh
+++ etc/sh/profile.d/dotfiles.sh
@@ -1,5 +1,3 @@
 # dotfiles
 
-if [ -d "$XDG_LIB_HOME/dotfiles/bin" ]; then
-	export PATH="$XDG_LIB_HOME/dotfiles/bin:$PATH"
-fi
+path prepend "$XDG_LIB_HOME/dotfiles/bin"
--- etc/sh/profile.d/utils.sh
+++ etc/sh/profile.d/utils.sh
@@ -1,5 +1,3 @@
 # utils
 
-if [ -d "$XDG_LIB_HOME/utils/bin" ]; then
-	export PATH="$XDG_LIB_HOME/utils/bin:$PATH"
-fi
+path prepend "$XDG_LIB_HOME/utils/bin"
