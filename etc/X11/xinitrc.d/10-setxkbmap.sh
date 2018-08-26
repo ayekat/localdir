6b5c13e8d053ba2fdc1b842f6b01f08ac6028dac
ayekat <takeya@bluewin.ch>
2018-08-26 12:40:45 +0200

    etc/X11/xinit: setxkbmap after a 1-second delay on kiwi
    I think there is even an issue for this for the moment - now it's
    official.

--- etc/X11/xinitrc.d/10-setxkbmap.sh
+++ etc/X11/xinitrc.d/10-setxkbmap.sh
@@ -1,5 +1,13 @@
 # Set X keyboard layout.
 
 if have setxkbmap; then
-	setxkbmap ayekat
+	case "$(uname -n)" in
+		(kiwi)
+			{
+				sleep 1
+				setxkbmap ayekat
+				notify-send 'X keymap set' 'ayekat'
+			} & ;;
+		(*) setxkbmap ayekat ;;
+	esac
 fi
