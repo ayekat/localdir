f7e18f3ba6cf266eaf978195d54dedb3858b78b4
ayekat <takeya@bluewin.ch>
2019-03-30 20:48:17 +0100

    etc/sh: Move environment setup from interactive to profile.d snippets

---
+++ etc/sh/profile.d/60-coreutils.sh
@@ -0,0 +1,7 @@
+# Coreutils
+
+# Friendlier colours for `ls`:
+LS_COLORS='fi=0:di=34:ex=32:ln=36:or=1;36:mi=1;31:bd=1;33:cd=33:pi=35:so=1;35'
+#          file dir   exec  link  orphan  orphan  block   char  pipe  socket
+#                                 link    target
+export LS_COLORS
---
+++ etc/sh/profile.d/60-gcc.sh
@@ -0,0 +1,4 @@
+# GNU Compiler Collection (GCC)
+
+# Friendlier colours:
+export GCC_COLORS='error=01;31:warning=33:note=36:caret=01;32:locus=37:quote=32'
