6fee61fde0503a5be6d86789a4614a77a1707389
ayekat <takeya@bluewin.ch>
2019-06-04 19:14:34 +0200

    etc/sh/profile.d/40-bf_sde.sh: Only extend PATH conditionally

--- etc/sh/profile.d/40-bf_sde.sh
+++ etc/sh/profile.d/40-bf_sde.sh
@@ -1,11 +1,14 @@
 # Barefoot SDE for P4 (Tolino)
 
 dir_sde="$HOME/sde"
+
 if [ -d "$dir_sde" ]; then
 	export SDE="$dir_sde"
 	export SDE_INSTALL="$SDE/install"
+
+	path append "$SDE"
+	path append "$SDE_INSTALL/bin"
+	path append "$HOME/tools"
 fi
 
-path append "$SDE"
-path append "$SDE/install/bin"
-path append "$HOME/tools"
+unset dir_sde
