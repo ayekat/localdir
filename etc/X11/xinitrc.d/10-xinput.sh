a236c0fa1cf5e4c4c3e9325295ba5fe8c0d9eb44
ayekat <takeya@bluewin.ch>
2019-06-26 15:32:03 +0200

    etc/X11/xinitrc.d/10-xinput.sh: Reorganise + fix for libinput 1.13.3
    For some reason, the mouse acceleration is super low, so I need to ramp
    it up manually. Also, rewrite the snippet to use xinput to remap the
    mouse buttons (so drop xmodmap as a requirement).
    
    Also make the device->id mapping a bit more elegant (instead of just a
    blunt and rather unreliable grep).

--- etc/X11/xinitrc.d/10-xinput.sh
+++ etc/X11/xinitrc.d/10-xinput.sh
@@ -3,12 +3,18 @@
 if have xinput; then
 	tpps='TPPS/2 IBM TrackPoint'
 	tpusb='pointer:Lenovo ThinkPad Compact USB Keyboard with TrackPoint'
-	mapbuttons() { xmodmap -e "pointer = 3 2 1 $(seq -s ' ' 4 $1)"; }
 
-	case "$(xinput list --name-only | grep "$tpps\\|$tpusb")" in
-		($tpps) mapbuttons 10 ;;
-		($tpusb) mapbuttons 22 ;;
-	esac
+	reverse_button_mapping() {
+		xinput --set-button-map $1 3 2 1 $(seq -s ' ' 4 $2)
+	}
+
+	if tpps_id=$(xinput list --id-only "$tpps" 2>/dev/null); then
+		reverse_button_mapping $tpps_id 10
+		xinput --set-prop $tpps_id 'libinput Accel Speed' '1.0' # libinput>=1.13.3
+	fi
+	if tpusb_id=$(xinput list --id-only "$tpusb" 2>/dev/null); then
+		reverse_button_mapping $tpusb_id 22
+	fi
 
 	unset tpps tpusb
 	unset -f mapbuttons
