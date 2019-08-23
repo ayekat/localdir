2edc7c8e1a96867f03b030df9d311ddf05b70b41
ayekat <takeya@bluewin.ch>
2019-08-23 14:16:47 +0200

    Add etc/arandr
    Currently there's a single layout for at home.

---
+++ etc/arandr/layouts/bremgarten.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output LVDS-1 --mode 1366x768 --pos 0x1080 --rotate normal --output VGA-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output HDMI-1 --off --output DP-1 --off --output HDMI-2 --off --output HDMI-3 --off --output DP-2 --off --output DP-3 --off
