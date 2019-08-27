ffdffdf2f81ad7d18324fa81ad3acfbac707073e
ayekat <takeya@bluewin.ch>
2019-08-27 17:58:31 +0200

    etc/X11: Make sure touchpad is disabled on X390
    For some reason, disabling the touchpad in the firmware interface has no
    effect.

--- etc/X11/xinitrc.d/10-xinput.sh
+++ etc/X11/xinitrc.d/10-xinput.sh
@@ -1,8 +1,12 @@
-# Swap left and right mouse buttons if using a ThinkPad keyboard.
+# Configure ThinkPad (and related) pointers:
+# * Swap left and right mouse button;
+# * Make sure that the touchpad is disabled.
 
 if have xinput; then
+(
 	tpps_x230='TPPS/2 IBM TrackPoint'
 	tpps_x390='Elan TrackPoint'
+	tpad_x390='Elan Touchpad'
 	tpusb='pointer:Lenovo ThinkPad Compact USB Keyboard with TrackPoint'
 
 	reverse_button_mapping() {
@@ -11,16 +15,19 @@ if have xinput; then
 
 	if tpps_id=$(xinput list --id-only "$tpps_x230" 2>/dev/null); then
 		reverse_button_mapping $tpps_id 10
-		xinput --set-prop $tpps_id 'libinput Accel Speed' '1.0' # libinput>=1.13.3
+		# XXX: libinput>=1.13.3
+		xinput --set-prop $tpps_id 'libinput Accel Speed' '1.0'
 	fi
 	if tpps_id=$(xinput list --id-only "$tpps_x390" 2>/dev/null); then
-		reverse_button_mapping $tpps_id 10
+		reverse_button_mapping $tpps_id 7
 		xinput --set-prop $tpps_id 'libinput Accel Speed' '-0.5'
+		if tpad_id=$(xinput list --id-only "$tpad_x390" 2>/dev/null); then
+			# XXX: disabling touchpad in firmware has not effect:
+			xinput --set-prop $tpad_id 'Device Enabled' 0
+		fi
 	fi
 	if tpusb_id=$(xinput list --id-only "$tpusb" 2>/dev/null); then
 		reverse_button_mapping $tpusb_id 22
 	fi
-
-	unset tpps_x230 tpps_x390 tpusb
-	unset -f reverse_button_mapping
+)
 fi
