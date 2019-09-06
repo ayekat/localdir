4cb82ed46926eaa7deb834973fd3c3855a10768f
ayekat <takeya@bluewin.ch>
2019-09-06 10:23:27 +0200

    Revert "etc/sh: "Fix" TERM on CentOS boxen"
    This reverts commit b6ceda3376ba01d03523bac3ab89f09bf19537fe.
    
    Turns out that CentOS is just waaaaaaaaay too outdated for these
    configuration files even remotely working correctly. I'm running into
    issues with tmux, git, pam, ...
    
    So instead of trying to fix this, let's just consider CentOS an
    unsupported host. This commit in particular can be replaced by just
    hardcoding TERM in the remote side's .bashrc.

--- etc/sh/interactive.d/term.sh
+++
@@ -1,6 +0,0 @@
-# Terminal-specific settings
-
-# I can't find a way to get the rxvt-unicode-256color terminfo in CentOS:
-if [ "$OS_NAME" = 'centos' ] && [ "$TERM" = 'rxvt-unicode-256color' ]; then
-	export TERM=rxvt-unicode
-fi
