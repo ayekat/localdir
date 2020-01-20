0a65fe4f50a86afa52990049ed52b3c678f25d7e
Tinu Weber <takeya@bluewin.ch>
2020-01-20 11:12:33 +0100

    etc/arandr: New third screen in office
    Now all my screens have the same resolution \o/

--- etc/arandr/.screenlayout/adsy.sh
+++ etc/arandr/.screenlayout/adsy.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output eDP-1 --mode 1920x1080 --pos 0x509 --rotate normal --output DP-1 --off --output DP-2 --off --output HDMI-1 --off --output DP-1-1 --primary --mode 1920x1080 --pos 1920x0 --rotate normal --output DP-1-2 --off --output DP-1-3 --mode 1680x1050 --pos 3840x30 --rotate normal
+xrandr --output eDP-1 --mode 1920x1080 --pos 0x509 --rotate normal --output DP-1 --off --output DP-2 --off --output HDMI-1 --off --output DP-1-1 --primary --mode 1920x1080 --pos 1920x0 --rotate normal --output DP-1-2 --mode 1920x1080 --pos 3840x0 --rotate normal --output DP-1-3 --off
