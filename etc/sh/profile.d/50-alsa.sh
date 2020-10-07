3d9e9c208c46f8e5075f87c7a6ebd630d26f5d9f
ayekat <takeya@bluewin.ch>
2020-10-07 19:55:24 +0200

    Switch to PulseAudio

--- etc/sh/profile.d/50-alsa.sh
+++
@@ -1,7 +0,0 @@
-# Force ALSA to use internal speakers on gurke.
-# (hacky workaround until we move to PulseAudio)
-
-if [ "$(uname -n)" = 'gurke' ]; then
-	export ALSA_CARD=PCH
-	systemctl --user import-environment ALSA_CARD
-fi
