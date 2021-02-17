aaf69c53cfcc211c985e135a331bc79b5bbb1133
Martin Weber <martin.weber@adfinis.com>
2021-02-17 13:47:27 +0100

    etc/karuiwm/profile.d: Fix shellcheck warnings and add TODO marker

--- etc/karuiwm/profile.d/10-xinput.sh
+++ etc/karuiwm/profile.d/10-xinput.sh
@@ -10,24 +10,24 @@ if command -v xinput >/dev/null; then
 	tpusb='pointer:Lenovo ThinkPad Compact USB Keyboard with TrackPoint'
 
 	reverse_button_mapping() {
-		xinput --set-button-map $1 3 2 1 $(seq -s ' ' 4 $2)
+		xinput --set-button-map $(($1)) 3 2 1 $(seq -s ' ' 4 $(($2)))
 	}
 
 	if tpps_id=$(xinput list --id-only "$tpps_x230" 2>/dev/null); then
-		reverse_button_mapping $tpps_id 10
+		reverse_button_mapping $((tpps_id)) 10
 		# XXX: libinput>=1.13.3
-		xinput --set-prop $tpps_id 'libinput Accel Speed' '1.0'
+		xinput --set-prop $((tpps_id)) 'libinput Accel Speed' '1.0'
 	fi
 	if tpps_id=$(xinput list --id-only "$tpps_x390" 2>/dev/null); then
-		reverse_button_mapping $tpps_id 7
-		xinput --set-prop $tpps_id 'libinput Accel Speed' '-0.3'
+		reverse_button_mapping $((tpps_id)) 7
+		xinput --set-prop $((tpps_id)) 'libinput Accel Speed' '-0.3'
 		if tpad_id=$(xinput list --id-only "$tpad_x390" 2>/dev/null); then
 			# XXX: disabling touchpad in firmware has not effect:
-			xinput --set-prop $tpad_id 'Device Enabled' 0
+			xinput --set-prop $((tpad_id)) 'Device Enabled' 0
 		fi
 	fi
 	if tpusb_id=$(xinput list --id-only "$tpusb" 2>/dev/null); then
-		reverse_button_mapping $tpusb_id 22
+		reverse_button_mapping $((tpusb_id)) 22
 	fi
 )
 fi
--- etc/karuiwm/profile.d/70-screenlock.sh
+++ etc/karuiwm/profile.d/70-screenlock.sh
@@ -1,4 +1,5 @@
 if command -v xset xss-lock /usr/share/karuiwm/scrlock >/dev/null; then
 	xset s 300
+	# TODO: what happens with previous instances of xss-lock?
 	xss-lock /usr/share/karuiwm/scrlock &
 fi
