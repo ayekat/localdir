7e0b5dc2d1744506a5c0ac542ebc2496a5df5de7
Martin Weber <martin.weber@adfinis.com>
2021-06-24 10:25:18 +0200

    etc/sh/interactive.d/90-ssh-agent.sh: Handle case of no agent running
    Also add a few more comments, and print the error output in case of
    retval=2.

--- etc/sh/interactive.d/90-ssh-agent.sh
+++ etc/sh/interactive.d/90-ssh-agent.sh
@@ -1,4 +1,10 @@
 if command -v ssh-add >/dev/null; then
+	# If not agent is running, we don't do anything:
+	if [ -z "${SSH_AUTH_SOCK:-}" ] || [ -z "${SSH_AGENT_PID:-}" ]; then
+		return
+	fi
+
+	# Define which key to be auto-loaded if necessary:
 	case "$(uname -n)" in
 		(gurke) _ssh_id=id_rsa ;;
 		(chirschi) _ssh_id=id_ed25519 ;;
@@ -6,13 +12,20 @@ if command -v ssh-add >/dev/null; then
 	if [ -z "${_ssh_id:-}" ]; then
 		return 0
 	fi
+
+	# Check the current state of loaded keys:
 	_ssh_add_retval=0
-	_ssh_add_list=$(ssh-add -l) || _ssh_add_retval=$?
+	_ssh_add_list=$(ssh-add -l 2>&1) || _ssh_add_retval=$?
 	case $_ssh_add_retval in
-		(0) ;;
-		(1) ssh-add ~/.ssh/"$_ssh_id" ;;
-		(2) printf '%s\n' "$_ssh_add_list" ;;
-		(*) printf 'Unknown exit code %d for ssh-add -l\n' $_ssh_add_retval ;;
+		(0) # at least one key is present, nothing to do
+			;;
+		(1) # no key present in the agent, add defined key
+			ssh-add ~/.ssh/"$_ssh_id" ;;
+		(2) # could not communicate with the agent, print error
+			printf '%s\n' "$_ssh_add_list" ;;
+		(*) # undocumented (should not happen), print error
+			printf 'Unknown exit code %d for ssh-add -l\n%s\n' \
+			       $_ssh_add_retval "$_ssh_add_list" ;;
 	esac
 	unset _ssh_id _ssh_add_retval _ssh_add_list
 fi
