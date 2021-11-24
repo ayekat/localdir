106aef1bcd84996665898f2d0b2b1bfb2e8a2b6a
ayekat <takeya@bluewin.ch>
2021-11-24 18:35:51 +0100

    etc/arandr: Update bremgarten.sh
    Turns out the integrated laptop screen is now called `eDP1` (without the
    dash).

--- etc/arandr/.screenlayout/bremgarten.sh
+++ etc/arandr/.screenlayout/bremgarten.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output eDP-1 --mode 1920x1080 --pos 0x1080 --rotate normal --output DP-1 --off --output HDMI-1 --off --output DP-2 --off --output HDMI-2 --primary --mode 1920x1080 --pos 0x0 --rotate normal
+xrandr --output eDP1 --mode 1920x1080 --pos 0x1080 --rotate normal --output DP1 --off --output DP2 --off --output HDMI1 --off --output HDMI2 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output VIRTUAL1 --off
