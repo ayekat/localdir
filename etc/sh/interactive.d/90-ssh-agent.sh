412366397cd3a33be70b982f93f35fc44f3e8b96
Martin Weber <martin.weber@adfinis.com>
2021-12-22 12:23:34 +0100

    etc/sh/ssh-agent: Assume id_ed25519 in all cases
    This removes the need for host-specific configuration. If the key isn't
    there, we won't load it; if the key is there, we probably want it
    anyway.

--- etc/sh/interactive.d/90-ssh-agent.sh
+++ etc/sh/interactive.d/90-ssh-agent.sh
@@ -1,15 +1,12 @@
 if command -v ssh-add >/dev/null; then
-	# If not agent is running, we don't do anything:
+	# If no agent is running, we don't do anything:
 	if [ -z "${SSH_AUTH_SOCK:-}" ] || [ -z "${SSH_AGENT_PID:-}" ]; then
 		return
 	fi
 
-	# Define which key to be auto-loaded if necessary:
-	case "$(uname -n)" in
-		(chirschi) _ssh_id=id_ed25519 ;;
-	esac
-	if [ -z "${_ssh_id:-}" ]; then
-		return 0
+	# Check if there's a key:
+	if [ ! -f ~/.ssh/id_ed25519 ]; then
+		return
 	fi
 
 	# Check the current state of loaded keys:
@@ -19,12 +16,12 @@ if command -v ssh-add >/dev/null; then
 		(0) # at least one key is present, nothing to do
 			;;
 		(1) # no key present in the agent, add defined key
-			ssh-add ~/.ssh/"$_ssh_id" ;;
+			ssh-add ~/.ssh/id_ed25519 ;;
 		(2) # could not communicate with the agent, print error
 			printf '%s\n' "$_ssh_add_list" ;;
 		(*) # undocumented (should not happen), print error
 			printf 'Unknown exit code %d for ssh-add -l\n%s\n' \
 			       $_ssh_add_retval "$_ssh_add_list" ;;
 	esac
-	unset _ssh_id _ssh_add_retval _ssh_add_list
+	unset _ssh_add_retval _ssh_add_list
 fi
