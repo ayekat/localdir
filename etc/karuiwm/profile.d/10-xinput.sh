b5f14569ba96ba80c119f991367f0fd10f65078f
ayekat <takeya@bluewin.ch>
2022-01-16 17:45:16 +0100

    etc/karuiwm/profile.d/10-xinput.sh: Fix duplicate device case
    For some reason, my external ThinkPad USB keyboard appears twice in
    xinput, so just doing a regular `xinput list --id-only "$name"` no
    longer works (xinput refuses to return anything if it matches multiple
    devices).
    
    After some testing, it turns out I need to apply the button reversing to
    both devices (no idea why), so this now adapts the 10-xinput.sh snippet
    to handle that.

--- etc/karuiwm/profile.d/10-xinput.sh
+++ etc/karuiwm/profile.d/10-xinput.sh
@@ -13,21 +13,80 @@ if command -v xinput >/dev/null; then
 		xinput --set-button-map $(($1)) 3 2 1 $(seq -s ' ' 4 $(($2)))
 	}
 
-	if tpps_id=$(xinput list --id-only "$tpps_x230" 2>/dev/null); then
-		reverse_button_mapping $((tpps_id)) 10
-		# XXX: libinput>=1.13.3
-		xinput --set-prop $((tpps_id)) 'libinput Accel Speed' '1.0'
+	# XXX Workaround for cases where a device appears multiple times. The
+	# manipulation needs be done on all devices, but xinput itself refuses to
+	# list multiple ID, so we have to do it ourselves.
+	xinput_list_ids() (
+		dname=$1
+		dfound=false
+
+		# Check if device type is specified:
+		case "$dname" in
+			(keyboard:*) dtype=keyboard; dname=${dname#keyboard:} ;;
+			(pointer:*) dtype=pointer; dname=${dname#pointer:} ;;
+			(*) dtype= ;;
+		esac
+
+		# Go through the available devices:
+		id_list=$(xinput --list --id-only) || return
+		for id in $id_list; do
+			case "$id" in (*[!0-9]*)
+				printf 'Non-numeric device ID: %s\n' "$id" >&2
+				return 1
+			esac
+			id=$((id))
+			name=$(xinput --list --name-only $id) || return
+			test "$name" = "$dname" || continue
+			if [ -n "$dtype" ]; then
+				# Name matches, but check if device type is what we want:
+				state=$(xinput --query-state $id) || return
+				case "$dtype" in
+					(keyboard) filter=KeyClass ;;
+					(pointer) filter=ButtonClass ;;
+				esac
+				while read -r line; do
+					case "$line" in ("$filter"|"$filter "*)
+						dfound=true
+						printf '%d\n' $id
+						break
+					esac
+				done <<- EOF
+				$state
+				EOF
+			else
+				# Name matches, no device type specified, remember this one:
+				dfound=true
+				printf '%d\n' $id
+			fi
+		done
+
+		# Return code (true==0 if found, false==1 if not found):
+		$dfound
+	)
+
+	if tpps_ids=$(xinput_list_ids "$tpps_x230"); then
+		for id in $tpps_ids; do
+			reverse_button_mapping "$id" 10
+			# XXX: libinput>=1.13.3
+			xinput --set-prop "$$id" 'libinput Accel Speed' '1.0'
+		done
 	fi
-	if tpps_id=$(xinput list --id-only "$tpps_x390" 2>/dev/null); then
-		reverse_button_mapping $((tpps_id)) 7
-		xinput --set-prop $((tpps_id)) 'libinput Accel Speed' '-0.3'
-		if tpad_id=$(xinput list --id-only "$tpad_x390" 2>/dev/null); then
+	if tpps_ids=$(xinput_list_ids "$tpps_x390"); then
+		for id in $tpps_ids; do
+			reverse_button_mapping "id" 7
+			xinput --set-prop "$id" 'libinput Accel Speed' '-0.3'
+		done
+	fi
+	if tpad_ids=$(xinput_list_ids "$tpad_x390"); then
+		for id in $tpad_ids; do
 			# XXX: disabling touchpad in firmware has not effect:
-			xinput --set-prop $((tpad_id)) 'Device Enabled' 0
-		fi
+			xinput --set-prop "$id" 'Device Enabled' 0
+		done
 	fi
-	if tpusb_id=$(xinput list --id-only "$tpusb" 2>/dev/null); then
-		reverse_button_mapping $((tpusb_id)) 22
+	if tpusb_ids=$(xinput_list_ids "$tpusb"); then
+		for id in $tpusb_ids; do
+			reverse_button_mapping "$id" 22
+		done
 	fi
 )
 fi
