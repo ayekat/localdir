4fcb816c0ba30c921668a57f87d2d8c792eae6f8
ayekat <takeya@bluewin.ch>
2023-10-29 17:53:12 +0100

    Reenable IBus
    Attempt with some additional urxvt options this time (as suggested in
    the Wiki).
    
    Not that I really believe this will fix the IBus segfaults, but...
    potentially this might avoid the freezes?

--- etc/karuiwm/profile.d/20-ibus.sh
+++ etc/karuiwm/profile.d/20-ibus.sh
@@ -1,6 +1,6 @@
-#if command -v ibus-daemon >/dev/null; then
-#	export GTK_IM_MODULE=ibus
-#	export QT_IM_MODULE=ibus
-#	export XMODIFIERS='@im=ibus'
-#	ibus-daemon -drx
-#fi
+if command -v ibus-daemon >/dev/null; then
+	export GTK_IM_MODULE=ibus
+	export QT_IM_MODULE=ibus
+	export XMODIFIERS='@im=ibus'
+	ibus-daemon -drx
+fi
